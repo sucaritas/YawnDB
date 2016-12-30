0dd9574e9c3b20703c8cff54cae075cc5fea28ef
Julio Cesar Saenz <julio.cesar.saenz@gmail.com>
2016-12-29 19:29:19 -0800

    spliting code bases on Yawn codebase and usage of yawn

---
+++ SampleUsage/SampleDataSchemas/ReferenceTo/Referencing.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YawnDB
+{
+    partial class Referencing<T> : YawnDB.ReferenceTo<T> where T : YawnDB.YawnSchema
+    {
+    }
+}
